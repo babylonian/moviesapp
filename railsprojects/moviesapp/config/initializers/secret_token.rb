86896f2229ed89d89e03cbfbe0cfc44fac866ba4
your name <youremail@example.com>
2015-01-20 14:35:15 +0000

    my first application

---
+++ railsprojects/moviesapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviesapp::Application.config.secret_token = 'b718ed7fe547a4a901c8585b993043fd7c882f44efb4bfe85553fee7f69e80e62e6d7d14a04d64b5947635f5ff294bb69fd5980a4e7bf89ac56024892d36249c'
